333b0f8471124f1006a4cc3c378e17d41806a897
Ken Harmon <harmkenn@gmail.com>
2024-10-02 11:03:39 +0000

    Update

--- Python_Madness_2025/apps/f_backpredict.py
+++ Python_Madness_2025/apps/f_backpredict.py
@@ -14,37 +14,37 @@ def app():
     p_year = st.slider('Year: ', 2008, 2023)
     
     if p_year == 2020:
-    st.markdown("No Bracket in 2020")
+        st.markdown("No Bracket in 2020")
     elif p_year != 2020:
     
-    FUPN = FUP.select_dtypes(exclude=['object'])
+        FUPN = FUP.select_dtypes(exclude=['object'])
+        
+        # Split the dataframe into features and targets
+        expl = FUPN.drop(['AFScore', 'AUScore'], axis=1)
+        respF = FUPN[['AFScore']]
+        respU = FUPN[['AUScore']]
+        
+        # Make Gradient Boosting Regressor models
+        model_F = GradientBoostingRegressor(random_state=42)
+        model_U = GradientBoostingRegressor(random_state=42)
+        
+        # Train the models on the entire dataset
+        model_F.fit(expl, respF)
+        model_U.fit(expl, respU)
+        
+        # Use the trained model to predict scores for the selected year
+        FUT = FUP[FUP['Year'] == p_year]
+        FUT = FUT.iloc[:, 0:10]
+        FUT['PFScore'] = model_F.predict(FUT.drop(['AFScore', 'AUScore'], axis=1))
+        FUT['PUScore'] = model_U.predict(FUT.drop(['AFScore', 'AUScore'], axis=1))
+        FUT.index = FUT.Game
     
-    # Split the dataframe into features and targets
-    expl = FUPN.drop(['AFScore', 'AUScore'], axis=1)
-    respF = FUPN[['AFScore']]
-    respU = FUPN[['AUScore']]
+        for x in range(1, 64):
+            FUT.loc[x, 'AWTeam'] = str(np.where(FUT.loc[x, 'AFScore'] >= FUT.loc[x, 'AUScore'], FUT.loc[x, 'AFTeam'], FUT.loc[x, 'AUTeam']))
+            FUT.loc[x, 'PWTeam'] = str(np.where(FUT.loc[x, 'PFScore'] >= FUT.loc[x, 'PUScore'], FUT.loc[x, 'AFTeam'], FUT.loc[x, 'AUTeam']))
+            FUT.loc[x, 'ESPN'] = np.where(FUT.loc[x, 'AWTeam'] == FUT.loc[x, 'PWTeam'], 10 * 2**(FUT.loc[x, 'Round'] - 1), 0)
     
-    # Make Gradient Boosting Regressor models
-    model_F = GradientBoostingRegressor(random_state=42)
-    model_U = GradientBoostingRegressor(random_state=42)
-    
-    # Train the models on the entire dataset
-    model_F.fit(expl, respF)
-    model_U.fit(expl, respU)
-    
-    # Use the trained model to predict scores for the selected year
-    FUT = FUP[FUP['Year'] == p_year]
-    FUT = FUT.iloc[:, 0:10]
-    FUT['PFScore'] = model_F.predict(FUT.drop(['AFScore', 'AUScore'], axis=1))
-    FUT['PUScore'] = model_U.predict(FUT.drop(['AFScore', 'AUScore'], axis=1))
-    FUT.index = FUT.Game
-    
-    for x in range(1, 64):
-      FUT.loc[x, 'AWTeam'] = str(np.where(FUT.loc[x, 'AFScore'] >= FUT.loc[x, 'AUScore'], FUT.loc[x, 'AFTeam'], FUT.loc[x, 'AUTeam']))
-      FUT.loc[x, 'PWTeam'] = str(np.where(FUT.loc[x, 'PFScore'] >= FUT.loc[x, 'PUScore'], FUT.loc[x, 'AFTeam'], FUT.loc[x, 'AUTeam']))
-      FUT.loc[x, 'ESPN'] = np.where(FUT.loc[x, 'AWTeam'] == FUT.loc[x, 'PWTeam'], 10 * 2**(FUT.loc[x, 'Round'] - 1), 0)
-    
-    TESPN = FUT['ESPN'].sum()
+        TESPN = FUT['ESPN'].sum()
     
     st.markdown('Total ESPN Score: ' + str(TESPN))
     FUT['Year'] = FUT['Year'].astype('str')
