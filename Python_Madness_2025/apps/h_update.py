c0efb581477b677ae3a85b92f9d2a18927110ebf
Ken Harmon <harmkenn@gmail.com>
2024-12-09 08:58:10 +0000

    Working ESPN

--- Python_Madness_2025/apps/h_update.py
+++ Python_Madness_2025/apps/h_update.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import streamlit as st
+from selenium import webdriver
+import io
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+import time
 
-def execute_code():
+def kenpom_code():
     # Code to be executed when the button is clicked
     # Load the CSV file
     df = pd.read_csv('Python_Madness_2025/data/Just2025.csv')
@@ -13,6 +22,40 @@ def execute_code():
     st.write("KenPom Data Updated")
     st.dataframe(df_concat) 
 
+def espnbpi_code():
+    service = Service(executable_path="chromedriver.exe")
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    driver = webdriver.Chrome(service=service,options=options)
+    y = 2024
+    driver.get(f'https://www.espn.com/mens-college-basketball/bpi/_/view/bpi/season/{y}')
+    driver.maximize_window()
+    time.sleep(10)
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler"))).click()
+
+    try:
+        for x in range(8):
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Show More"))).click() 
+            time.sleep(5)
+    except:
+        try:
+            element = WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "//table")))
+            with io.StringIO(driver.page_source) as f:
+                df = pd.read_html(f)[0]
+                df2 = pd.read_html(f)[1]
+        finally:
+            
+            bpi = pd.concat([df, df2], axis=1)
+            bpi['Year']=y
+            bpi = bpi.iloc[:,[12,0,6,7]]
+            bpi.columns = ['Year','Team','BPI(O)','BPI(D)']
+            print(y)
+    espnBPI = pd.read_csv('step02_espnbpi0824.csv')
+    espnBPI = espnBPI[espnBPI['Year']<y]   
+    espnBPI = pd.concat([espnBPI,bpi]) 
+
+    espnBPI.to_csv('step02_espnbpi0825.csv',index=False)      
+
 # Display the button
 if st.button("Update Data"):
-    execute_code()
+    kenpom_code()
